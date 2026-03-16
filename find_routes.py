653f91687237f41cf7ba9cf005e06ff63b48a54f
engelwillem <90029371+engelwillem@users.noreply.github.com>
2026-03-16 20:28:01 +0700

    chore(governance): clean up root directory from stray output and script files

--- find_routes.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-app_dir = r"e:\thechoosentalksnext\src\app"
-for root, dirs, files in os.walk(app_dir):
-    for d in dirs:
-        if "[" in d and "]" in d:
-            print(os.path.relpath(os.path.join(root, d), app_dir))
