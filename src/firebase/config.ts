6a9f497138699ef028c110e90e897bff10b50cab
engelwillem <90029371+engelwillem@users.noreply.github.com>
2026-05-16 11:30:39 +0800

    ci: switch gitleaks to native CLI and restore firebase build key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6804627896-ab6c8",
   "appId": "1:566846844376:web:9aa5f2b4efca44d672d6ad",
-  "apiKey": "",
+  "apiKey": "AIzaSyCiJtTGqrd3Sup-XzhRo64pbzkOTzyWHAM",
   "authDomain": "studio-6804627896-ab6c8.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "566846844376"
